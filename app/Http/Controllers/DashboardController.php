29d04073524b6be531436bca999e929341d311ba
thegreytangent <thegreytangent@gmail.com>
2022-10-18 22:21:53 +0800

    Feat: Done display dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,13 +1,41 @@
 <?php
 
-namespace App\Http\Controllers;
+    namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\View\View;
+    use App\Models\StockItem;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\DB;
+    use Illuminate\View\View;
 
-class DashboardController extends Controller
-{
-    public function index() : View {
-        return view('dashboard.index');
+    class DashboardController extends Controller
+    {
+
+
+        public function index()
+        {
+
+            $noOfCategories = DB::table('stock_categories')->count();
+            $noOfItems = DB::table('stock_items')->count();
+            $noOfUsers = 4;
+
+            $data = [];
+            $category = [];
+
+            foreach (StockItem::all() as $item) {
+                $data[] = $item->getBalance();
+                $category[] = $item->item_name;
+            }
+
+
+
+            return view('dashboard.index')->with([
+                'number_of_categories' => $noOfCategories,
+                'number_of_items'      => $noOfItems,
+                'number_of_users'      => $noOfUsers,
+                'chart'                => [
+                    'data'       => $data,
+                    'categories' => $category
+                ]
+            ]);
+        }
     }
-}
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,9 +1,135 @@
 @extends('template.main')
 
 
+@push('styles')
+    <link rel="stylesheet" href="{{asset("assets/css/icon.css")}}">
+@endpush
+
 @section('content')
     <div class="col-span-12 mt-8">
-        <h3>Under Development</h3>
+        <h3>Dashboard</h3>
     </div>
 
+    <div class="col-12 col-lg-12">
+        <div class="row">
+{{--            <div class="col-1"></div>--}}
+            <div class="col-6 col-lg-4 col-md-6">
+                <div class="card">
+                    <div class="card-body px-4 py-4-5">
+                        <div class="row">
+                            <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
+                                <div class="stats-icon purple mb-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box2" viewBox="0 0 16 16">
+                                        <path d="M2.95.4a1 1 0 0 1 .8-.4h8.5a1 1 0 0 1 .8.4l2.85 3.8a.5.5 0 0 1 .1.3V15a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V4.5a.5.5 0 0 1 .1-.3L2.95.4ZM7.5 1H3.75L1.5 4h6V1Zm1 0v3h6l-2.25-3H8.5ZM15 5H1v10h14V5Z"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                            <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                <h6 class="text-muted font-semibold">
+                                    Stock Categories
+                                </h6>
+                                <h6 class="font-extrabold mb-0">{{$number_of_categories}}</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-6 col-lg-3 col-md-6">
+                <div class="card">
+                    <div class="card-body px-4 py-4-5">
+                        <div class="row">
+                            <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
+                                <div class="stats-icon green mb-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-boxes" viewBox="0 0 16 16">
+                                        <path d="M7.752.066a.5.5 0 0 1 .496 0l3.75 2.143a.5.5 0 0 1 .252.434v3.995l3.498 2A.5.5 0 0 1 16 9.07v4.286a.5.5 0 0 1-.252.434l-3.75 2.143a.5.5 0 0 1-.496 0l-3.502-2-3.502 2.001a.5.5 0 0 1-.496 0l-3.75-2.143A.5.5 0 0 1 0 13.357V9.071a.5.5 0 0 1 .252-.434L3.75 6.638V2.643a.5.5 0 0 1 .252-.434L7.752.066ZM4.25 7.504 1.508 9.071l2.742 1.567 2.742-1.567L4.25 7.504ZM7.5 9.933l-2.75 1.571v3.134l2.75-1.571V9.933Zm1 3.134 2.75 1.571v-3.134L8.5 9.933v3.134Zm.508-3.996 2.742 1.567 2.742-1.567-2.742-1.567-2.742 1.567Zm2.242-2.433V3.504L8.5 5.076V8.21l2.75-1.572ZM7.5 8.21V5.076L4.75 3.504v3.134L7.5 8.21ZM5.258 2.643 8 4.21l2.742-1.567L8 1.076 5.258 2.643ZM15 9.933l-2.75 1.571v3.134L15 13.067V9.933ZM3.75 14.638v-3.134L1 9.933v3.134l2.75 1.571Z"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                            <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                <h6 class="text-muted font-semibold">Stock Items</h6>
+                                <h6 class="font-extrabold mb-0">{{$number_of_items}}</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-lg-3 col-md-6">
+                <div class="card">
+                    <div class="card-body px-4 py-4-5">
+                        <div class="row">
+                            <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
+                                <div class="stats-icon red mb-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
+                                        <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z"/>
+                                    </svg>
+                                </div>
+                            </div>
+                            <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                <h6 class="text-muted font-semibold">No. of Users</h6>
+                                <h6 class="font-extrabold mb-0">{{$number_of_users}}</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Stock Item Balances</h4>
+                    </div>
+                    <div class="card-body">
+                        <div id="chart-profile-visit" style="min-height: 315px;"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
 @endsection
+
+@push('scripts')
+    <script src="{{asset('assets/extensions/apexcharts/apexcharts.min.js')}}"></script>
+    <script>
+
+
+
+        let chartProfileVisit = new ApexCharts(
+            document.querySelector("#chart-profile-visit"),
+            {
+                annotations: {
+                    position: "back",
+                },
+                dataLabels: {
+                    enabled: false,
+                },
+                chart: {
+                    type: "bar",
+                    height: 300,
+                },
+                fill: {
+                    opacity: 1,
+                },
+                plotOptions: {},
+                series: [
+                    {
+                        name: "balances",
+                        data: <?php echo json_encode($chart['data']); ?>,
+                    },
+                ],
+                colors: "#435ebe",
+                xaxis: {
+                    categories: <?php echo json_encode($chart['categories']); ?>,
+                },
+            }
+        )
+
+        chartProfileVisit.render();
+
+
+    </script>
+@endpush
